47bb1836f208ff2c6796634f1604b7e6885bcd5d
Claudijo <claudijo.borovic@gmail.com>
2014-01-16 01:25:05 +0100

    Add polyfill for Object.create.

---
+++ src/polyfill/create.js
@@ -0,0 +1,16 @@
+// Allowing use of much of the functionality of Object.create in implementations
+// that do not natively support it.
+// See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+if (!Object.create) {
+  Object.create = (function(){
+    function F(){}
+
+    return function(o){
+      if (arguments.length != 1) {
+        throw new Error('Object.create implementation only accepts one parameter.');
+      }
+      F.prototype = o
+      return new F()
+    }
+  })()
+}
