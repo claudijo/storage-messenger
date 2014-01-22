647411a1d32757275ae57661256eda256b432033
Claudijo <claudijo.borovic@gmail.com>
2014-01-22 23:28:01 +0100

    Force update of gh-pages.

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+#if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
 
   #copy data we're interested in to other place
@@ -23,4 +23,4 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git push -fq origin gh-pages > /dev/null
 
   echo -e "Done magic with coverage\n"
-fi
+#fi
