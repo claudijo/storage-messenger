8c89ed2f3204a13f4f5a0bab8eea1590803481c9
Travis <travis@travis-ci.org>
2014-01-31 20:05:04 +0100

    Update gh-pages on both push an pull requests.

--- bin/update-gh-pages.sh
+++ bin/update-gh-pages.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update gh-pages\n"
+echo -e "Starting to update gh-pages\n"
 
-  #copy data we're interested in to other place
-  cp -R coverage $HOME/coverage
+#copy data we're interested in to other place
+cp -R coverage $HOME/coverage
 
-  #go to home and setup git
-  cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis"
+#go to home and setup git
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
 
-  #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@${GH_REF} gh-pages > /dev/null
+#using token clone gh-pages branch
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@${GH_REF} gh-pages > /dev/null
 
-  #go into directory and copy data we're interested in to that directory
-  cd gh-pages
-  cp -Rf $HOME/coverage/**/* coverage/
+#go into directory and copy data we're interested in to that directory
+cd gh-pages
+cp -Rf $HOME/coverage/**/* coverage/
 
-  #add, commit and push files
-  git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+#add, commit and push files
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
 
-  echo -e "Done magic with coverage\n"
-fi
+echo -e "Done magic with coverage\n"
