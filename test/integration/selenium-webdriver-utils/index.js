ae35813775959e9c64b420d2296f2c6f0dd055df
Travis <travis@travis-ci.org>
2014-02-15 22:48:01 +0100

    Update comments.

--- test/integration/selenium-webdriver-utils/index.js
+++ test/integration/selenium-webdriver-utils/index.js
@@ -8,13 +8,13 @@ exports.openWindow = function(driver, url, name) {
 };
 
 // Helper to get localStorage size, since driver.getLocalStorageSize() is not
-// implemented in WebDriverJS 2.37.0
+// implemented in WebDriverJS as of this writing.
 exports.getLocalStorageSize = function(driver) {
   return driver.executeScript('return window.localStorage.length');
 };
 
 // Helper to get localStorage size, since driver.getLocalStorageItem() is not
-// implemented in WebDriverJS 2.37.0
+// implemented in WebDriverJS as of this writing.
 exports.getLocalStorageItem = function(driver ,key) {
   return driver.executeScript('return window.localStorage.getItem(arguments[0])',
       key);
