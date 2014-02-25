bb68514cc0dd913cf7f5c6dc257b8e0c7ec9891f
Travis <travis@travis-ci.org>
2014-02-25 22:42:09 +0100

    Add bulk of available browsers on Sauce Labs for integration tests.

--- test/integration/config/sauce.config.js
+++ test/integration/config/sauce.config.js
@@ -8,8 +8,64 @@ module.exports = {
   browsersToTest: [
     {
       browserName: 'firefox',
+      platform: 'Windows 7',
+      version: '27'
+    }, {
+      browserName: 'firefox',
+      platform: 'Windows 7',
+      version: '3.5'
+    }, {
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    }, {
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: '10'
+    }, {
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: '9'
+    }, {
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: '8'
+    }, {
+      browserName: 'safari',
+      platform: 'OS X 10.9',
+      version: '7'
+    }, {
+      browserName: 'safari',
+      platform: 'OS X 10.8',
+      version: '6'
+    }, {
+      browserName: 'safari',
       platform: 'OS X 10.6',
-      version: '25'
+      version: '5'
+    },  {
+      browserName: 'chrome',
+      platform: 'Windows 7',
+      version: '32'
+    }, {
+      browserName: 'opera',
+      platform: 'Windows 7',
+      version: '12'
+    }, {
+      browserName: 'opera',
+      platform: 'Windows 7',
+      version: '11'
+    }, {
+      browserName: 'ipad',
+      platform: 'OS X 10.6',
+      version: '4'
+    }, {
+      browserName: 'andriod',
+      platform: 'linux',
+      version: '4.3'
+    }, {
+      browserName: 'andriod',
+      platform: 'linux',
+      version: '4.0'
     }
   ]
 };
